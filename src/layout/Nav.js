df89848a5ec8769333340709037e69a9c276c929
Paulina Słabosz <sejmipool@gmail.com>
2024-10-01 20:11:26 +0200

    Nav: add logo picture

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../images/logo.png';
 
 function Nav() {
   return (
     <>
-      <div>logo</div>
+      <div>
+        <img src={logo} alt='Text Sage Valley' />
+      </div>
       <ul>
         <li>Start</li>
         <li>Big Cabin</li>
