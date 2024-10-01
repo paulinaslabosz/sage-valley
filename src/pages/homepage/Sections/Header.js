f07675c801fd23218ff321e5679483479baa2b66
Paulina Słabosz <sejmipool@gmail.com>
2024-10-01 22:44:45 +0200

    Header: set width and height of the video

--- src/pages/homepage/Sections/Header.js
+++ src/pages/homepage/Sections/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './styles/Header.scss';
 import video from '../../../images/headerVideo.mp4';
 
 function Header() {
   return (
-    <>
+    <div className='header_wrapper'>
       <video muted autoPlay={'autoplay'} preLoad='auto' loop>
         <source src={video} type='video/mp4' />
       </video>
-    </>
+    </div>
   );
 }
 
