86830eca357479727af2556a562647284ecc0685
Paulina Słabosz <sejmipool@gmail.com>
2024-10-03 17:26:11 +0200

    Cabins: add images

--- src/pages/homepage/Sections/Cabins.js
+++ src/pages/homepage/Sections/Cabins.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import bigCabin from '../../../images/BigCabin.jpg';
+import littleCabin from '../../../images/LittleCabin.jpg';
 
 function Cabins() {
   return (
     <div>
       <div>
-        <img src='' alt='Big cabin' />
+        <img src={bigCabin} alt='Big cabin' />
         <div>
           <button>Click to see</button>
         </div>
       </div>
       <div>
-        <img src='' alt='Little cabin' />
+        <img src={littleCabin} alt='Little cabin' />
         <div>
           <button>Click to see</button>
         </div>
