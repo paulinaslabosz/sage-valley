01faf77814c2e8627cf7aad569e07a06bf1a5430
Paulina Słabosz <sejmipool@gmail.com>
2024-10-03 21:16:10 +0200

    Cabins: add styles for cabins images and link, add gap between sections on homepage

--- src/pages/homepage/Sections/Cabins.js
+++ src/pages/homepage/Sections/Cabins.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import './styles/Cabins.scss';
 import bigCabin from '../../../images/BigCabin.jpg';
 import littleCabin from '../../../images/LittleCabin.jpg';
 
 function Cabins() {
   return (
-    <div>
-      <div>
+    <div className='cabins_wrapper'>
+      <div className='cabins_cabin'>
         <img src={bigCabin} alt='Big cabin' />
         <div>
-          <button>Click to see</button>
+          <a href='#'>SEE MORE</a>
         </div>
       </div>
-      <div>
+      <div className='cabins_cabin'>
         <img src={littleCabin} alt='Little cabin' />
         <div>
-          <button>Click to see</button>
+          <a href='#'>SEE MORE</a>
         </div>
       </div>
     </div>
