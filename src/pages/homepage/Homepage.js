f8dc2aeeb2d8fe484107170972451d2e9b2904eb
Paulina Słabosz <sejmipool@gmail.com>
2024-10-03 17:20:00 +0200

    Homepage: created Cabins component and basic structure

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Sections/Header';
+import Cabins from './Sections/Cabins';
 function Homepage() {
   return (
     <>
       <Header />
+      <Cabins />
     </>
   );
 }
---
+++ src/pages/homepage/Sections/Cabins.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Cabins() {
+  return (
+    <div>
+      <div>
+        <img src='' alt='Big cabin' />
+        <div>
+          <button>Click to see</button>
+        </div>
+      </div>
+      <div>
+        <img src='' alt='Little cabin' />
+        <div>
+          <button>Click to see</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cabins;
