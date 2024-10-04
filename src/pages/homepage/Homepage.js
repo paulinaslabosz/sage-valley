83a1608f4dd9ceaa2184d1b7b4f113633cba0c39
Paulina Słabosz <sejmipool@gmail.com>
2024-10-04 22:25:38 +0200

    Icons: create and add component, basic structure without content

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Sections/Header';
 import Cabins from './Sections/Cabins';
 import Content from './Sections/Content';
+import Icons from './Sections/Icons';
+
 function Homepage() {
   return (
     <>
       <Header />
       <Cabins />
       <Content />
+      <Icons />
     </>
   );
 }
---
+++ src/pages/homepage/Sections/Icons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Icons() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Icons;
