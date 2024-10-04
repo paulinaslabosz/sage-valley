01da7115475b87e0a69f6353ae5023b7f8cdcfa4
Paulina Słabosz <sejmipool@gmail.com>
2024-10-04 16:09:58 +0200

    Content: add styles for sections

--- src/pages/homepage/Sections/Content.js
+++ src/pages/homepage/Sections/Content.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './styles/Content.scss';
 import intimacyImg from '../../../images/intimacy.jpg';
 import peaceImg from '../../../images/peace.jpg';
 
 function Content() {
   return (
-    <div>
-      <div>
-        <div>
-          <img src={peaceImg} alt='Cabin photo with lake' />
-          <p>Peace</p>
+    <div className='content_wrapper'>
+      <div className='content_section'>
+        <div className='content_section-img'>
+          <img src={peaceImg} alt='Cabin with lake' />
+          <p className='title_first'>Peace</p>
         </div>
-        <div>
-          <h2>Title</h2>
+        <div className='content_section-description section_first'>
+          <h2>
+            Sage
+            <br />
+            Valley{' '}
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -20,19 +25,23 @@ function Content() {
           </p>
         </div>
       </div>
-      <div>
-        <div>
-          <img src={intimacyImg} alt='Cabin photo at night' />
-          <p>Intimacy</p>
-        </div>
-        <div>
-          <h2>Title</h2>
+      <div className='content_section'>
+        <div className='content_section-description section_second'>
+          <h2>
+            Sage
+            <br />
+            Valley{' '}
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
+        </div>
+        <div className='content_section-img'>
+          <img src={intimacyImg} alt='Cabin at night' />
+          <p className='title_second'>Intimacy</p>
         </div>
       </div>
     </div>
