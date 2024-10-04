9009acc7214a5bf659d6e1e3cbace02ef6825f66
Paulina Słabosz <sejmipool@gmail.com>
2024-10-04 14:35:28 +0200

    Content: create and add component, add basic structure

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Sections/Header';
 import Cabins from './Sections/Cabins';
+import Content from './Sections/Content';
 function Homepage() {
   return (
     <>
       <Header />
       <Cabins />
+      <Content />
     </>
   );
 }
---
+++ src/pages/homepage/Sections/Content.js
@@ -0,0 +1,39 @@
+import React from 'react';
+function Content() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src='' alt='' />
+          <p>Peace</p>
+        </div>
+        <div>
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src='' alt='' />
+          <p>Intimacy</p>
+        </div>
+        <div>
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
