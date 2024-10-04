8d9590201a782ea69e0dc8d5464788571f43f0f0
Paulina Słabosz <sejmipool@gmail.com>
2024-10-04 23:07:58 +0200

    Icons: install package from fontawesome, style icons, add titles

--- src/pages/homepage/Sections/Icons.js
+++ src/pages/homepage/Sections/Icons.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import './styles/Icons.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSquareParking,
+  faWifi,
+  faTree,
+  faDog,
+} from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
 function Icons() {
   return (
     <div className='icons_wrapper'>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <FontAwesomeIcon icon={faSquareParking} />
+          <p>Free parking</p>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faSquare} />
+          <p>Fenced area</p>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faWifi} />
+          <p>Free WiFi</p>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faTree} />
+          <p>Lot of greenery</p>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faDog} />
+          <p>Animals accepted</p>
+        </li>
       </ul>
     </div>
   );
