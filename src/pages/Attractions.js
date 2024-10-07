eac7fb008ab5e49be055acb647c355d0bc387f0e
Paulina Słabosz <sejmipool@gmail.com>
2024-10-07 22:33:55 +0200

    Attractions: create styles for content and bookmarks

--- src/pages/Attractions.js
+++ src/pages/Attractions.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Attractions.scss';
 function Attractions() {
   return (
-    <>
+    <div className='atr_wrapper'>
       <ul className='atr_nav'>
         <li className='atr_navlink'>BATHHOUSE</li>
-        <li>SAUNA</li>
-        <li>MASSAGE</li>
-        <li>FOOD</li>
-        <li>GREEN</li>
+        <li className='atr_navlink'>SAUNA</li>
+        <li className='atr_navlink'>MASSAGE</li>
+        <li className='atr_navlink'>FOOD</li>
+        <li className='atr_navlink'>GREEN</li>
       </ul>
-      <div className='atr_wrapper'>Tu treść</div>
-    </>
+      <div className='atr_content'>Tu treść</div>
+    </div>
   );
 }
 
