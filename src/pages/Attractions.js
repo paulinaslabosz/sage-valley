604945aefb884677c93620aaa7e613c6657294c0
Paulina Słabosz <sejmipool@gmail.com>
2024-10-07 21:51:58 +0200

    Attractions: add structure of the component

--- src/pages/Attractions.js
+++ src/pages/Attractions.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Attractions() {
-  return <div>attractions</div>;
+  return (
+    <>
+      <ul>
+        <li>BATHHOUSE</li>
+        <li>SAUNA</li>
+        <li>MASSAGE</li>
+        <li>FOOD</li>
+        <li>GREEN</li>
+      </ul>
+      <div>Tu treść</div>
+    </>
+  );
 }
 
 export default Attractions;
