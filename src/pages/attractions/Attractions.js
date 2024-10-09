4e291f5c68873ad61d8dccab3eb43d89f6809c41
Paulina Słabosz <sejmipool@gmail.com>
2024-10-09 22:38:23 +0200

    Attractions: add nav links for sections, add styles for active

--- src/pages/attractions/Attractions.js
+++ src/pages/attractions/Attractions.js
@@ -1,16 +1,71 @@
 import React from 'react';
 import Section from './Section';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import './Attractions.scss';
 function Attractions() {
   return (
     <div className='atr_wrapper'>
       <ul className='atr_nav'>
-        <li className='atr_navlink'>BATHHOUSE</li>
-        <li className='atr_navlink'>SAUNA</li>
-        <li className='atr_navlink'>MASSAGE</li>
-        <li className='atr_navlink'>FOOD</li>
-        <li className='atr_navlink'>GREEN</li>
+        <NavLink
+          to='/attractions/tub'
+          className={({ isActive, isPending }) =>
+            isPending
+              ? 'atr_navlink'
+              : isActive
+              ? 'atr_navlink atr_active'
+              : 'atr_navlink'
+          }
+        >
+          HOT TUB
+        </NavLink>
+        <NavLink
+          to='/attractions/sauna'
+          className={({ isActive, isPending }) =>
+            isPending
+              ? 'atr_navlink'
+              : isActive
+              ? 'atr_navlink atr_active'
+              : 'atr_navlink'
+          }
+        >
+          SAUNA
+        </NavLink>
+        <NavLink
+          to='/attractions/massage'
+          className={({ isActive, isPending }) =>
+            isPending
+              ? 'atr_navlink'
+              : isActive
+              ? 'atr_navlink atr_active'
+              : 'atr_navlink'
+          }
+        >
+          MASSAGE
+        </NavLink>
+        <NavLink
+          to='/attractions/food'
+          className={({ isActive, isPending }) =>
+            isPending
+              ? 'atr_navlink'
+              : isActive
+              ? 'atr_navlink atr_active'
+              : 'atr_navlink'
+          }
+        >
+          FOOD
+        </NavLink>
+        <NavLink
+          to='/attractions/green'
+          className={({ isActive, isPending }) =>
+            isPending
+              ? 'atr_navlink'
+              : isActive
+              ? 'atr_navlink atr_active'
+              : 'atr_navlink'
+          }
+        >
+          GREEN
+        </NavLink>
       </ul>
       <div className='atr_content'>
         <Routes>
