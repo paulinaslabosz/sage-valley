1293fb0bd1f10d121e9f2884fe97b9dcdc6249c5
Paulina Słabosz <sejmipool@gmail.com>
2024-10-11 19:48:07 +0200

    Attractions: change sauna img name, add basic function for changing section state

--- src/pages/attractions/Section.js
+++ src/pages/attractions/Section.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Section.scss';
 import tub from '../../images/attractions/hottub.jpg';
@@ -9,30 +9,35 @@ import green from '../../images/attractions/green.jpg';
 
 const sections = [
   {
+    name: 'tub',
     title: 'Hot Tub',
     content:
       "Whether you're looking for a traditional wellness experience or a cozy getaway, this summer cottage with its authentic banya and hot tub is the perfect destination for relaxation and rejuvenation.Heated to a soothing temperature, the hot tub provides a perfect escape to melt away stress, while surrounded by the fresh air and natural beauty of the countryside. The combination of warm water and the peaceful ambiance makes it the perfect spot for deep relaxation, whether during the day amidst the birdsong or at night under the stars.",
     img: tub,
   },
   {
+    name: 'sauna',
     title: 'Sauna',
     content:
       'Our cozy sauna provides the perfect place to unwind and rejuvenate after a day of exploration or relaxation. Located within our guesthouse, the sauna offers a warm, peaceful atmosphere where you can detoxify, relieve stress, and improve overall well-being. Whether you prefer a quiet session or a social moment with friends, our sauna is the ideal escape to refresh both body and mind. Enjoy the therapeutic benefits of heat, while surrounded by comfort and tranquility.',
     img: sauna,
   },
   {
+    name: 'massage',
     title: 'Massage',
     content:
       'Indulge in a soothing massage at our guesthouse, designed to relax your body and calm your mind. Our professional massage services help relieve tension, reduce stress, and promote overall well-being. Whether you seek a gentle, relaxing treatment or a deep tissue massage, our experienced therapists will tailor the session to your needs. Unwind in a serene environment, and let the healing hands of our specialists rejuvenate your muscles and restore balance to your day.',
     img: massage,
   },
   {
+    name: 'food',
     title: 'Food',
     content:
       'Experience the taste of the region with delicious homemade meals offered in the area. Many nearby restaurants and eateries serve traditional, hearty dishes made with fresh, local ingredients, giving you an authentic flavor of the local cuisine. For a perfect start to your day, we also offer breakfast baskets filled with locally sourced products. Enjoy fresh bread, cheeses, meats, and other seasonal delights from local farms and producers, all delivered to your door. It’s a great way to savor the region’s culinary heritage in the comfort of your room or while exploring the beautiful surroundings.',
     img: food,
   },
   {
+    name: 'green',
     title: 'Green',
     content:
       "Our guesthouse is nestled in a serene environment, surrounded by lush greenery that creates a peaceful and refreshing atmosphere. The area is rich with natural beauty, offering a perfect escape from the hustle and bustle of daily life. Whether you're relaxing in the garden or taking a stroll around the property, you'll be immersed in nature's tranquility, with trees, plants, and open green spaces all around. It's the ideal setting for those who love fresh air, scenic views, and the calming presence of nature.",
@@ -42,7 +47,12 @@ const sections = [
 
 function Section() {
   const { id } = useParams();
-  console.log(id);
+  const [section, setSection] = useState(id);
+
+  useEffect(() => {
+    setSection(id);
+  });
+
   return (
     <>
       <div className='section_wrapper'>
