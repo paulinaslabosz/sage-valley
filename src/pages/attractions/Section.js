7e4496c0e7a542f4e09ab434c288376e3c29286a
Paulina Słabosz <sejmipool@gmail.com>
2024-10-14 21:07:07 +0200

    Attractions: add arrays for sliders

--- src/pages/attractions/Section.js
+++ src/pages/attractions/Section.js
@@ -14,6 +14,14 @@ const sections = [
     content:
       "Whether you're looking for a traditional wellness experience or a cozy getaway, this summer cottage with its authentic banya and hot tub is the perfect destination for relaxation and rejuvenation.Heated to a soothing temperature, the hot tub provides a perfect escape to melt away stress, while surrounded by the fresh air and natural beauty of the countryside. The combination of warm water and the peaceful ambiance makes it the perfect spot for deep relaxation, whether during the day amidst the birdsong or at night under the stars.",
     img: tub,
+    slider: [
+      <img src='./images/balia/bania1.jpg' alt='Bania' />,
+      <img src='./images/balia/bania2.jpg' alt='Bania' />,
+      <img src='./images/balia/bania3.jpg' alt='Bania' />,
+      <img src='./images/balia/bania4.jpg' alt='Bania' />,
+      <img src='./images/balia/bania5.jpg' alt='Bania' />,
+      <img src='./images/balia/bania6.jpg' alt='Bania' />,
+    ],
   },
   {
     name: 'sauna',
