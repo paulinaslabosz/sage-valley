66f4a7f5ff2f89e59ec6b693585c9e65e532e7b7
Paulina Słabosz <sejmipool@gmail.com>
2024-10-14 21:40:36 +0200

    Attractions: add styles for sliders

--- src/pages/attractions/Slider.js
+++ src/pages/attractions/Slider.js
@@ -5,7 +5,12 @@ function Slider({ images }) {
   const sliderElements = images.map((image, index) => (
     <img src={image.props.src} alt={image.props.alt} key={index} />
   ));
-  return <div>{sliderElements}</div>;
+  return (
+    <div className='atr_slider'>
+      <div className='slider-track'>{sliderElements}</div>
+      <div className='slider-track'>{sliderElements}</div>
+    </div>
+  );
 }
 
 export default Slider;
