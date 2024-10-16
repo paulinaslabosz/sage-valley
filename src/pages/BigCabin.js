079b649be18def32c36c554783ec90a67dc36b10
Paulina Słabosz <sejmipool@gmail.com>
2024-10-16 22:04:20 +0200

    Cabin: create structure of the component

--- src/pages/BigCabin.js
+++ src/pages/BigCabin.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function BigCabin() {
-  return <div>big cabin</div>;
+  return (
+    <>
+      <h2>Title</h2>
+      <div>
+        <div>
+          <div>
+            <div>piktogramy</div>
+            <div>opis</div>
+          </div>
+          <div>
+            <img src='' alt='' />
+          </div>
+        </div>
+        <div>GALERIA</div>
+      </div>
+    </>
+  );
 }
 
 export default BigCabin;
