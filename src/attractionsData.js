0f06dcdfe78dcd96c5d26322df2e35cf5e1a5921
Paulina Słabosz <sejmipool@gmail.com>
2024-10-17 22:27:00 +0200

    Data: create two files with data

---
+++ src/attractionsData.js
@@ -0,0 +1,73 @@
+export const attractions = [
+  {
+    name: 'tub',
+    title: 'Hot Tub',
+    content:
+      "Whether you're looking for a traditional wellness experience or a cozy getaway, this summer cottage with its authentic banya and hot tub is the perfect destination for relaxation and rejuvenation.Heated to a soothing temperature, the hot tub provides a perfect escape to melt away stress, while surrounded by the fresh air and natural beauty of the countryside. The combination of warm water and the peaceful ambiance makes it the perfect spot for deep relaxation, whether during the day amidst the birdsong or at night under the stars.",
+    img: tub,
+    slider: [
+      <img src='/images/balia/bania1.jpg' alt='Bania' />,
+      <img src='/images/balia/bania2.jpg' alt='Bania' />,
+      <img src='/images/balia/bania3.jpg' alt='Bania' />,
+      <img src='/images/balia/bania4.jpg' alt='Bania' />,
+      <img src='/images/balia/bania5.jpg' alt='Bania' />,
+      <img src='/images/balia/bania6.jpg' alt='Bania' />,
+    ],
+  },
+  {
+    name: 'sauna',
+    title: 'Sauna',
+    content:
+      'Our cozy sauna provides the perfect place to unwind and rejuvenate after a day of exploration or relaxation. Located within our guesthouse, the sauna offers a warm, peaceful atmosphere where you can detoxify, relieve stress, and improve overall well-being. Whether you prefer a quiet session or a social moment with friends, our sauna is the ideal escape to refresh both body and mind. Enjoy the therapeutic benefits of heat, while surrounded by comfort and tranquility.',
+    img: sauna,
+    slider: [
+      <img src='/images/sauna/sauna1.jpg' alt='Sauna' />,
+      <img src='/images/sauna/sauna2.jpg' alt='Sauna' />,
+      <img src='/images/sauna/sauna3.jpg' alt='Sauna' />,
+      <img src='/images/sauna/sauna4.jpg' alt='Sauna' />,
+      <img src='/images/sauna/sauna5.jpg' alt='Sauna' />,
+    ],
+  },
+  {
+    name: 'massage',
+    title: 'Massage',
+    content:
+      'Indulge in a soothing massage at our guesthouse, designed to relax your body and calm your mind. Our professional massage services help relieve tension, reduce stress, and promote overall well-being. Whether you seek a gentle, relaxing treatment or a deep tissue massage, our experienced therapists will tailor the session to your needs. Unwind in a serene environment, and let the healing hands of our specialists rejuvenate your muscles and restore balance to your day.',
+    img: massage,
+    slider: [
+      <img src='/images/massage/massage1.jpg' alt='Massage' />,
+      <img src='/images/massage/massage2.jpg' alt='Massage' />,
+      <img src='/images/massage/massage3.jpg' alt='Massage' />,
+      <img src='/images/massage/massage4.jpg' alt='Massage' />,
+      <img src='/images/massage/massage5.jpg' alt='Massage' />,
+    ],
+  },
+  {
+    name: 'food',
+    title: 'Food',
+    content:
+      'Experience the taste of the region with delicious homemade meals offered in the area. Many nearby restaurants and eateries serve traditional, hearty dishes made with fresh, local ingredients, giving you an authentic flavor of the local cuisine. For a perfect start to your day, we also offer breakfast baskets filled with locally sourced products. Enjoy fresh bread, cheeses, meats, and other seasonal delights from local farms and producers, all delivered to your door. It’s a great way to savor the region’s culinary heritage in the comfort of your room or while exploring the beautiful surroundings.',
+    img: food,
+    slider: [
+      <img src='/images/food/food1.jpg' alt='Food' />,
+      <img src='/images/food/food2.jpg' alt='Food' />,
+      <img src='/images/food/food3.jpg' alt='Food' />,
+      <img src='/images/food/food4.jpg' alt='Food' />,
+    ],
+  },
+  {
+    name: 'green',
+    title: 'Green',
+    content:
+      "Our guesthouse is nestled in a serene environment, surrounded by lush greenery that creates a peaceful and refreshing atmosphere. The area is rich with natural beauty, offering a perfect escape from the hustle and bustle of daily life. Whether you're relaxing in the garden or taking a stroll around the property, you'll be immersed in nature's tranquility, with trees, plants, and open green spaces all around. It's the ideal setting for those who love fresh air, scenic views, and the calming presence of nature.",
+    img: green,
+    slider: [
+      <img src='/images/green/green1.jpg' alt='Green' />,
+      <img src='/images/green/green2.jpg' alt='Green' />,
+      <img src='/images/green/green3.jpg' alt='Green' />,
+      <img src='/images/green/green4.jpg' alt='Green' />,
+      <img src='/images/green/green5.jpg' alt='Green' />,
+      <img src='/images/green/green6.jpg' alt='Green' />,
+    ],
+  },
+];
---
+++ src/cabinData.js
@@ -0,0 +1,45 @@
+export const cabinData = [
+  {
+    name: 'big cabin',
+    title: 'Big Cabin',
+    // img: wstawić ścieżkę do zdjęcia
+    details: {
+      people: '10-12',
+      beds: '10',
+      bathrooms: '3',
+    },
+    facilities: [
+      'iron',
+      'oven',
+      'microwave',
+      'dishwasher',
+      'cutlery and plates',
+      'coffee machine',
+      'clothes dryer',
+      'hair dryer',
+      'air conditioning',
+      'TV',
+    ],
+    // gallery: linki do zdjęc - galeria
+  },
+
+  {
+    name: 'little cabin',
+    title: 'Little Cabin',
+    // img: wstawić ścieżkę do zdjęcia
+    details: {
+      people: '4-6',
+      beds: '4',
+      bathrooms: '2',
+    },
+    facilities: [
+      'iron',
+      'oven',
+      'cutlery and plates',
+      'clothes dryer',
+      'hair dryer',
+      'air conditioning',
+    ],
+    // gallery: linki do zdjęc - galeria
+  },
+];
