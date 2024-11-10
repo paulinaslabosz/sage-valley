941993a5efade652ea6b51c166323ff96618b808
Paulina Słabosz <sejmipool@gmail.com>
2024-11-10 18:56:38 +0100

    Lightbox: change classess names

--- src/pages/Cabin/Lightbox.js
+++ src/pages/Cabin/Lightbox.js
@@ -6,25 +6,25 @@ function Lightbox({ location, images }) {
   const [showLine, setShowLine] = useState(true);
 
   const gallery = images.map((image) => (
-    <div className='lightbox_gallery-image'>
+    <div className='gallery_list-image'>
       <img src={image.src} alt={image.alt} />
     </div>
   ));
 
   return (
-    <div className='lightbox_wrapper'>
-      <div className='lightbox_title'>
-        <h3 className='lightbox_title-text'>Gallery</h3>
+    <div className='gallery_wrapper'>
+      <div className='gallery_title'>
+        <h3 className='gallery_title-text'>Gallery</h3>
         <CSSTransition
           in={showLine}
           key={location.key}
           classNames='line-grow'
           timeout={1600}
         >
-          <div className='lightbox_title-line line'></div>
+          <div className='gallery_title-line line'></div>
         </CSSTransition>
       </div>
-      <div className='lightbox_gallery'>{gallery}</div>
+      <div className='gallery_list'>{gallery}</div>
     </div>
   );
 }
