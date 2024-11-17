08f7d9029e4c9ac45909164457f4dc6f710bdbba
Paulina Słabosz <sejmipool@gmail.com>
2024-11-17 19:12:22 +0100

    HamMenu: add animation visible/hidden

--- src/layout/HamburgerMenu.js
+++ src/layout/HamburgerMenu.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './styles/HamburgerMenu.scss';
 function HamburgerMenu() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  console.log(isMenuOpen);
   return (
     <>
-      <FontAwesomeIcon icon={faBars} />
-      <ul className='ham_list'>
-        <li>
+      <FontAwesomeIcon icon={faBars} onClick={() => handleMenu()} />
+      <ul className={isMenuOpen ? 'ham_list' : 'ham_list-hide'}>
+        <li className='ham_list-el'>
           <NavLink to='/'>Start</NavLink>
         </li>
 
-        <li>
+        <li className='ham_list-el'>
           <NavLink to='bigCabin'>Big Cabin</NavLink>
         </li>
-        <li>
+        <li className='ham_list-el'>
           <NavLink to='littleCabin'>Little Cabin</NavLink>
         </li>
-        <li>
+        <li className='ham_list-el'>
           <NavLink to='attractions/tub'>Attractions</NavLink>
         </li>
-        <li>
+        <li className='ham_list-el'>
           <NavLink to='contact'>Contact</NavLink>
         </li>
       </ul>
