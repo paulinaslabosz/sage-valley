c3df87dfa60bdd5423c00ae5e153a0319338492f
Paulina Słabosz <sejmipool@gmail.com>
2024-11-20 22:58:19 +0100

    HamMenu: FIXED close menu when click on icon, close menu when click on link

--- src/layout/HamburgerMenu.js
+++ src/layout/HamburgerMenu.js
@@ -1,19 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './styles/HamburgerMenu.scss';
 function HamburgerMenu() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const ref = useRef(null);
-
-  const handleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+  const buttonRef = useRef(null);
+  const location = useLocation();
+  console.log('start');
+  console.log(isMenuOpen);
 
   useEffect(() => {
     const handleOutsideClick = (e) => {
-      if (!ref.current?.contains(e.target)) {
+      if (
+        !ref.current?.contains(e.target) &&
+        !buttonRef.current?.contains(e.target)
+      ) {
         setMenuOpen(false);
       }
     };
@@ -21,10 +24,26 @@ function HamburgerMenu() {
     return () => {
       window.removeEventListener('mousedown', handleOutsideClick);
     };
-  }, [ref]);
+  }, [ref, buttonRef]);
+
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location]);
+
+  console.log(isMenuOpen);
+  const handleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+  console.log(isMenuOpen);
+  console.log('Stop');
   return (
     <>
-      <FontAwesomeIcon icon={faBars} onClick={() => handleMenu()} />
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={handleMenu}
+        onBlur={() => setMenuOpen(false)}
+        ref={buttonRef}
+      />
       <ul className={isMenuOpen ? 'ham_list' : 'ham_list-hide'} ref={ref}>
         <li className='ham_list-el'>
           <NavLink to='/'>Start</NavLink>
